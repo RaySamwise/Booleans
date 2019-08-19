653693f81e5d87ff77d576f60f5d84138bc07c0d
Ray <r.samwise@gmail.com>
2019-08-19 16:13:07 +0400

    start

---
+++ src/Loader.java
@@ -0,0 +1,32 @@
+
+public class Loader
+{
+    public static void main(String[] args)
+    {
+        int milkAmount = 252; // ml
+        int powderAmount = 500; // g
+        int eggsCount = 5; // items
+        int sugarAmount = 25; // g
+        int oilAmount = 11; // ml
+        int appleCount = 8;
+
+        //powder - 400 g, sugar - 10 g, milk - 1 l, oil - 30 ml
+        if (powderAmount >= 400 && sugarAmount >=10 && milkAmount >= 200 && oilAmount >= 30)
+            {
+                System.out.println("Pancakes");
+            }
+        //milk - 300 ml, powder - 5 g, eggs - 5
+        if (powderAmount >= 5 && eggsCount >=5 && milkAmount >= 300)
+        {
+            System.out.println("Omelette");
+        }
+
+
+        //apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
+        if (powderAmount >= 300 && appleCount >= 3 && milkAmount >= 100 && eggsCount >= 4)
+        {
+            System.out.println("Apple pie");
+        }
+
+    }
+}
